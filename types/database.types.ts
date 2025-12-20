c1e96cb2d583ece81cc983a34f95a6b2b639fed2
Dhruv2311 <dhruv.sonagra.cg@gmail.com>
2025-12-20 11:33:39 +0530

    Add placeholder database types

---
+++ types/database.types.ts
@@ -0,0 +1,9 @@
+// Database types will be generated from Supabase
+// For now, we'll use a placeholder type
+export type Database = {
+    public: {
+        Tables: {
+            [key: string]: any
+        }
+    }
+}
